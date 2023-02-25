de18221c8d197c1808b1af317086052f8562e1a7
Mateus Magalhães <95831951+MagalhaesDev@users.noreply.github.com>
2023-02-25 14:04:26 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const body = document.getElementsByTagName("body")[0]
+const form = document.getElementsByTagName("form")[0];
+const optionA = document.getElementById("optionA");
+const optionB = document.getElementById("optionB");
+const message = document.createElement("div");
+message.classList.add("message");
+body.appendChild(message);
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if(optionB.value > optionA.value) {
+    message.innerHTML = "Fomulário valido";
+    message.classList.add("valid")
+    } else {
+    message.innerHTML = "Fomulário invalido";
+    message.classList.add("invalid")
+    }
+
+    optionA.value = "";
+    optionB.value = "";
+})
